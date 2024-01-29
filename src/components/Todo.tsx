5837f9b86984c349361fb3cff735cef9d2eec599
AlexOlg <alexolgui11@gmail.com>
2024-01-29 17:46:34 -0300

    Initial commit

---
+++ src/components/Todo.tsx
@@ -0,0 +1,28 @@
+// import { useState } from "react"
+import {TodoId, type Todo as TodoType } from "../types"
+
+
+interface Props extends TodoType {
+    onRemoveTodo: ({id}: TodoId) => void
+    onToggleCompletedTodo:  ({id, completed} : Pick<TodoType, 'id'| 'completed'>) => void
+}
+
+export const Todo: React.FC<Props> = ({title, id, completed, onRemoveTodo, onToggleCompletedTodo}) => {
+    // const [todoCompleted, setTodoCompleted] = useState({completed})
+    return(
+        <div className="view">
+            <input  
+            className="toggle"
+            type="checkbox"
+            checked={completed}
+            onChange={(e) => {onToggleCompletedTodo({id, completed: e.target.checked})}}
+            />
+            <label>{title}</label>
+            <button 
+            className="destroy"
+            onClick={() => {onRemoveTodo({id})}}
+            />
+        </div>
+    )
+
+}
